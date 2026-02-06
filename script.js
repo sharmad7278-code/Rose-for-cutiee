b7726751e0638e53ff213e41ad2452482aff05af
sharmad7278-code <sharmad7278@gmail.com>
2026-02-07 05:27:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ function showMsg() {
   document.getElementById("msg").innerText =
     "You are the most beautiful rose 🌹❤️";
 }
+setInterval(() => {
+  const rose = document.createElement("div");
+  rose.innerText = "🌹";
+  rose.className = "rose";
+  rose.style.left = Math.random() * 100 + "vw";
+  rose.style.animationDuration = (3 + Math.random() * 3) + "s";
+  document.body.appendChild(rose);
+
+  setTimeout(() => {
+    rose.remove();
+  }, 6000);
+}, 300);
